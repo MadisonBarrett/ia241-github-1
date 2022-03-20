13cda1ebeb28c343e0accab53a58f3a696e5b9c8
EC2 Default User <ec2-user@ip-172-31-84-241.ec2.internal>
2022-03-20 15:20:27 +0000

    lab7

---
+++ lec7.py
@@ -0,0 +1,26 @@
+'''
+lec7
+while loop
+'''
+
+i=5
+while i >=0 :
+    
+    i = i -1
+    
+    if i ==3:
+        pass
+    
+    print(i)
+    
+while i >=0:
+    try:
+        print(1/(i-3))
+    except:
+        pass
+    i=i-1
+
+
+
+
+    
